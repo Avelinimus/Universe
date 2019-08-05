bf3790d94c23a82d474abba4ade81872a635a80c
Avelinimus <avel63146@gmail.com>
2019-08-05 22:29:32 +0300

    :D
    :D

---
+++ Scripts/Background.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
